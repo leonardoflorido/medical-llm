d7288901b05bfd7986afc847bc9fd8566cc2db58
Leonardo Flórido <leonardoflorido@proton.me>
2024-09-11 15:40:27 +0100

    Update the project.

--- main.py
+++ main.py
@@ -1,19 +1,44 @@
-from scripts.fine_tune import fine_tune
-from scripts.inference import generate_text
+import torch
+from trl import SFTTrainer
+from peft import LoraConfig
+from datasets import load_dataset
+from transformers import (AutoModelForCausalLM, AutoTokenizer, BitsAndBytesConfig, TrainingArguments, pipeline)
 
 def main():
-    model_name = "aboonaji/llama2finetune-v2"
-    train_dataset_path = "aboonaji/wiki_medical_terms_llam2_format"
-    output_dir = "./results"
+    # Loading the model
+    llama_model = AutoModelForCausalLM.from_pretrained(
+        pretrained_model_name_or_path = "aboonaji/llama2finetune-v2",
+        quantization_config = BitsAndBytesConfig(load_in_4bit = True,
+        bnb_4bit_compute_dtype = getattr(torch, "float16"),
+        bnb_4bit_quant_type = "nf4"))
+    llama_model.config.use_cache = False
+    llama_model.config.pretraining_tp = 1
+
+    # Loading the tokenizer
+    llama_tokenizer = AutoTokenizer.from_pretrained(
+        pretrained_model_name_or_path = "aboonaji/llama2finetune-v2", 
+        trust_remote_code = True)
+    llama_tokenizer.pad_token = llama_tokenizer.eos_token
+    llama_tokenizer.padding_side = "right"
+
+    # Setting the training arguments
+    training_arguments = TrainingArguments(
+        output_dir = "./results", 
+        per_device_train_batch_size = 4, 
+        max_steps = 100)
     
-    # Fine-tune the model
-    model, tokenizer = fine_tune(model_name, train_dataset_path, output_dir)
+    # Supervised Fine-Tuning trainer
+    llama_sft_trainer = SFTTrainer(
+        model = llama_model, 
+        args = training_arguments, 
+        train_dataset = load_dataset(path = "aboonaji/wiki_medical_terms_llam2_format", split = "train"), 
+        tokenizer = llama_tokenizer,
+        peft_config = LoraConfig(task_type = "CASUAL_LM", r = 64, lora_alpha = 16, lora_dropout = 0.1), 
+        dataset_text_field = "text")
     
-    # Example interaction with the model
-    user_prompt = "Please tell me about Ascariasis"
-    response = generate_text(model, tokenizer, user_prompt)
-    
-    print(response)
+    # Training the model
+    llama_sft_trainer.train()
 
 if __name__ == "__main__":
     main()
+    
